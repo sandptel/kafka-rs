2c8d796905c8bf28867b40a0442c20dc1888626a
sandptel <sandeeppatel626735@gmail.com>
2025-02-17 13:46:28 +0530

    writing to a tcp connection

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read,Write};
+
+struct Header{
+    // request_api_key : i16,
+    // request_api_version:i16,
+    correlation_id:i32,
+    // client_id: String,
+}
+
+struct Message{
+    message_size : u32,
+    body:String,
+    header: Header,
+}
+
+fn handle_client(mut stream: TcpStream)
+{
+    let mut buf = [0,0,0,0,0,0,0,7];
+    stream.write(&buf).unwrap();
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -13,6 +33,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
